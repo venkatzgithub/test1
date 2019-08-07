0729b45899ea3574f26ef60272d36922cfc8468a
venkatzgithub <imvenkatprasad@gmail.com>
2019-08-07 11:21:31 +0900

    Updates

---
+++ precache-manifest.33c28de3fff07c27142ad34a02c061bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d61669c9e32d1fe5c15ef0c398af854",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "bda0cf58e189499dbd7a",
+    "url": "/test1/static/css/main.3582b02b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/test1/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bda0cf58e189499dbd7a",
+    "url": "/test1/static/js/main.1fac61b7.chunk.js"
+  },
+  {
+    "revision": "110df05cd91b572a9bfe",
+    "url": "/test1/static/js/runtime~main.044b3335.js"
+  }
+]);
